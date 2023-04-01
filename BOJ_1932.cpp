98de15a35cfa1f93b1eda2e83bae11c06b9cb87c
figurekim317 <figurekim317@gmail.com>
2023-04-01 23:03:41 +0900

    Add files via upload

---
+++ BOJ_1932.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<vpii> vvpii;
+typedef vector<vpll> vvpll;
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define MOD 998244353
+
+int n,m,a[510][510];
+
+void Solve()
+{
+	cin>>n;
+	for(int i=n; i--; )
+	{
+		for(int j=n-i,k; j--; )
+			cin>>k,
+			a[i][j]=max(a[i+1][j],a[i+1][j-1])+k;
+	}
+	cout<<*max_element(a[0],a[0]+n);
+}
+
+void Init(bool isLocal){ if(isLocal) freopen("input.txt","r",stdin);
+	ios::sync_with_stdio(false); cin.tie(NULL);
+}
+int main(){ Init(01);
+//	int tc; cin>>tc; for(int i=1;i<=tc;i++)
+//	cout<<"Case #"<<i<<'\n',
+	Solve();
+	return 0;
+}
