a0ae582b54825e7cf14ac48af72b4dc9815877b7
figurekim317 <figurekim317@gmail.com>
2023-04-01 23:01:44 +0900

    Add files via upload

---
+++ BOJ_1874.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<vpii> vvpii;
+typedef vector<vpll> vvpll;
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define MOD 998244353
+
+int n,m=1;
+vi a;
+stack<int> b;
+string ans;
+
+void Solve()
+{
+	cin>>n;
+	a.resize(n);
+	for(auto &i: a) cin>>i;
+	for(auto &i: a)
+	{
+		if(m>i)
+		{
+			if(b.empty() || b.top()!=i)
+			{
+				cout<<"NO";
+				return;
+			}
+			else b.pop(), ans+="-\n";
+		}
+		else
+		{
+			for(; m<=i; m++)
+				b.push(m), ans+="+\n";
+			b.pop(); ans+="-\n";
+		}
+	}
+	cout<<ans;
+}
+
+void Init(bool isLocal){ if(isLocal) freopen("input.txt","r",stdin);
+	ios::sync_with_stdio(false); cin.tie(NULL);
+}
+int main(){ Init(01);
+//	int tc; cin>>tc; for(int i=1;i<=tc;i++)
+//	cout<<"Case #"<<i<<'\n',
+	Solve();
+	return 0;
+}
