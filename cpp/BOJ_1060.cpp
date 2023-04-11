8ef56eead811f67be1d6ed0f4b6e0f250a3b4337
Jongwan Kim <figurekim317@gmail.com>
2023-04-11 23:21:55 +0900

    Update BOJ_1060.cpp

--- cpp/BOJ_1060.cpp
+++ cpp/BOJ_1060.cpp
@@ -1,4 +1,7 @@
-#include <bits/stdc++.h>
+#i#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;nclude <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef pair<int,int> pii;
