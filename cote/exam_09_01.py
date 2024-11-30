d4e215ffb9b92210be5a6e134e9da611fbe720d0
Dohyeong Kim <figurekim317@gmail.com>
2024-11-30 15:39:31 +0900

    no message

--- cote/exam_09_01.py
+++ cote/exam_09_01.py
@@ -35,6 +35,32 @@ input = sys.stdin.readline
 INF = int(1e9)
 
 n, m = map(int, input().split())
+start = int(input())
+graph = [[] for i in range(n+1)]
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for candidate in graph[now]:
+            cost = dist + candidate[1]
+            if cost < distance[candidate[0]]:
+                distance[candidate[0]] = cost
+                heapq.heappush(q, (cost, candidate[0]))
+
+
+
+n, m = map(int, input().split())
 
 start = int(input())
 graph = [[] for i in range(n+1)]
