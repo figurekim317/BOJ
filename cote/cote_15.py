d95b27dd467db1ae63d3b90147a3461418fe2207
Dohyeong Kim <figurekim317@gmail.com>
2024-11-30 16:04:38 +0900

    no message

--- cote/cote_15.py
+++ cote/cote_15.py
@@ -1,4 +1,47 @@
 # 못풀었음
+import heapq
+
+INF = int(1e9)
+n, m, k, x = map(int, input().split())
+graph = [[] for i in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append((b, 1))
+
+distance = [INF]*(n+1)
+
+def dijkstra(start):
+    queue = []
+    heapq.heappush(queue, (0, start))
+    distance[start] = 0
+
+    while queue:
+        dist, now = heapq.heappop(queue)
+        if distance[now] < dist:
+            continue
+        for candidate in graph[now]:
+            cost = dist + candidate[1]
+            if cost < distance[candidate[0]]:
+                distance[candidate[0]] = cost
+                heapq.heappush(queue, (cost, candidate[0]))
+
+dijkstra(x)
+
+check = False
+for i in range(1, n+1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if check == False:
+    print(-1)
+
+#print(distance)
+
+
+
+
+'''
 from collections import deque
 
 n, m, k, x = map(int, input().split())
@@ -28,4 +71,4 @@ for i in range(1, n+1):
         check = True
 
 if check == False:
-    print(-1)
+    print(-1)'''
